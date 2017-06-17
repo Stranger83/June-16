ef949224f6597b8d8624ea63ce04c0da917902bb
July Manova <jmanova83@gmail.com>
2017-06-17 16:55:47 +0300

    01-02/07 solved

---
+++ OddFilter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddFilter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+			var evenOnly = new List<int>();
+			for (int i = 0; i < input.Count; i++)
+			{
+				if (input[i] % 2 == 0)
+				{
+					evenOnly.Add(input[i]);
+				}
+			
+			}
+
+			var av = evenOnly.Average();
+
+			for (int i = 0; i < evenOnly.Count; i++)
+			{
+				if (evenOnly[i] > av)
+				{
+					evenOnly[i] += 1;
+				}
+				else
+				{
+					evenOnly[i] -= 1;
+				}
+			}
+			Console.WriteLine(string.Join(" ", evenOnly));
+		}
+	}
+}
