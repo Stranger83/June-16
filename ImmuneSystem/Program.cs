bd972d8095c2bc944bc484300971489af2198d82
July Manova <jmanova83@gmail.com>
2017-06-17 19:27:35 +0300

    03-04/07 solved

---
+++ ImmuneSystem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmuneSystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var startHealth = int.Parse(Console.ReadLine());
+			var command = Console.ReadLine();
+			var virusStrength = new Dictionary<string, int>();
+			var currentHealth = startHealth;
+			while (command != "end")
+			{
+				var strength = command.ToCharArray().Select(x => (int)x).Sum() / 3;
+				var timeToDefeat = 0;
+				
+				if (!virusStrength.ContainsKey(command))
+				{
+					virusStrength[command] = strength;
+					timeToDefeat = strength * command.Length;
+				}
+				else
+				{
+					timeToDefeat = strength * command.Length / 3;
+				}
+
+				Console.WriteLine($"Virus {command}: {virusStrength[command]} => {timeToDefeat} seconds");
+
+				currentHealth -= timeToDefeat;
+				
+				if (currentHealth <= 0)
+				{
+					Console.WriteLine("Immune System Defeated.");
+					return;
+				}
+								
+				Console.WriteLine($"{command} defeated in {timeToDefeat/60}m {timeToDefeat%60}s");
+				Console.WriteLine($"Remaining health: {currentHealth}");
+				currentHealth = (int)(currentHealth * 1.2);
+				if (currentHealth > startHealth)
+				{
+					currentHealth = startHealth;
+				}
+				command = Console.ReadLine();
+			}
+			Console.WriteLine($"Final Health: {currentHealth}");
+		}
+	}
+}
---
+++ SupermarketDatabase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketDatabase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine();
+			var productPrice = new Dictionary<string, double>();
+			var productQty = new Dictionary<string, int>();
+
+			while (input != "stocked")
+			{
+				var tokens = input.Split(' ');
+				var product = tokens[0];
+		        var price = double.Parse(tokens[1]);
+				var quantity = int.Parse(tokens[2]);
+
+				if (!productPrice.ContainsKey(product)) {
+					productPrice[product] = 0;
+				}
+				productPrice[product] = price;
+
+				if (!productQty.ContainsKey(product))
+				{
+					productQty[product] = 0;
+				}
+				productQty[product] += quantity;
+								
+				input = Console.ReadLine();
+			}
+			var total = 0.0;
+			foreach (var kvp in productPrice)
+			{
+				var prod = kvp.Key;
+				var price = kvp.Value;
+				var qty = productQty[prod];
+				Console.WriteLine($"{prod}: ${price:f2} * {qty} = ${price * qty:f2}");
+				total += price * qty;
+			}
+
+			Console.WriteLine("------------------------------");
+			Console.WriteLine($"Grand Total: ${total:f2}");
+		}
+	}
+}
